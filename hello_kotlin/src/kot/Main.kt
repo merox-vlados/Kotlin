066ed0b6bdef0613e301523ffec1c76a1caecf59
merox-vlados <merox-vlados@mail.ru>
2023-08-25 12:28:40 +0300

    commit 25.08.2023

--- hello_kotlin/src/kot/Main.kt
+++ hello_kotlin/src/kot/Main.kt
@@ -3,9 +3,19 @@ package kot
 
 
 fun main() {
-    val a: String? = null;
-    val b: String? = "3456";
-    val c: String? = "";
-    val result: Int = (a?.length?:0) + (b?.length ?: 0) + (c?.length ?: 0);
-    println(result)
+
+
+//    var count = 600
+//    val food = if( count > 500) {
+//        count -= 500
+//        "Пицца"
+//    } else if (count > 300) {
+//        count -= 300
+//       "Шаурма"
+//    } else {
+//        count -= 30
+//       "Доширак"
+//    }
+//    println("Вы можете позволить себе купить следующий продукт: $food")
+
 }
