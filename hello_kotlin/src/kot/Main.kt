aa7a88f2a20ab4e04baac1c2426131f33d9f2fa0
merox-vlados <merox-vlados@mail.ru>
2023-08-28 18:51:51 +0300

    commit 28.08.2023

--- hello_kotlin/src/kot/Main.kt
+++ hello_kotlin/src/kot/Main.kt
@@ -3,26 +3,9 @@ package kot
 
 
 fun main() {
-    val numbers = mutableListOf<Int>(5,8,12,-2,6,5)
-    val result = sort(8,-6,5,88)
-    for(i in result) {
-       println(i)
-    }
-
+    volume(5, 6)
 }
 
-fun sort(numbers : MutableList<Int>): List<Int> {
-    for(i in 1 until numbers.size) {
-        for(j in numbers.size - 1 downTo i) {
-            if(numbers[j] < numbers[j - 1]) {
-                val temp = numbers[j]
-                numbers[j] = numbers[j -1]
-                numbers[j-1] = temp
-            }
-        }
-    }
-    return numbers
+fun volume(length: Int , width: Int = length, height: Int = length) {
+    println(length * width * height)
 }
-
-fun sort(numbers: Array<Int>) = kot.sort(numbers.toMutableList())
-fun sort(vararg numbers: Int) = kot.sort(numbers.toMutableList())
