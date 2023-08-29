b41660813fdfffb97b9d43836ae09d7b9bac204d
merox-vlados <merox-vlados@mail.ru>
2023-08-29 17:35:28 +0300

    commit 29.08.2023

--- hello_kotlin/src/kot/Main.kt
+++ hello_kotlin/src/kot/Main.kt
@@ -3,9 +3,26 @@ package kot
 
 
 fun main() {
-    volume(5, 6)
+    val perimeter: (Int, Int) -> Int = { a,b -> (a+b) * 2}
+    println(perimeter(5,6))
+    val name: (String) -> Unit = { println("Hello, $it!")}
+    name("Vlad")
+    val sort: (Array<Int>) -> Array<Int> = {
+        for(i in it.size - 2 downTo 0) {
+            for(j in 0..1) {
+                if(it[j] < it[j+1]) {
+                    val temp = it[j]
+                    it[j] = it[j + 1]
+                    it[j + 1] = temp
+                }
+            }
+        }
+        it
+    }
+    val sortedArray = sort(arrayOf(-56,3,10,-2,34,-33))
+    for(i in sortedArray) {
+        println(i)
+    }
 }
 
-fun volume(length: Int , width: Int = length, height: Int = length) {
-    println(length * width * height)
-}
+
