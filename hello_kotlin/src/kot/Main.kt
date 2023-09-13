1aaa923c7641d32714b98812ea84ff83f8cb5a87
merox-vlados <merox-vlados@mail.ru>
2023-09-13 17:35:30 +0300

    commit 13.09.2023

--- hello_kotlin/src/kot/Main.kt
+++ hello_kotlin/src/kot/Main.kt
@@ -2,14 +2,32 @@ package kot
 
 
 fun main() {
-    val array = generateSequence ("Сотрудник №1") {
-        val index = it.substring(11).toInt()
-        "Сотрудник №${index+1}"
-    }
-    val listOfFirst100 = array.take(100)
 
-    for(employee in listOfFirst100) {
-        println(employee)
+    val array = mutableListOf<String>(
+        "Годенко Владислав",
+        "Ляшко Родион",
+        "Гаджиев Рамин",
+        "Черепанов Родион",
+        "Зяблицкий Максим")
+    val arrayOfLastNames = array.map{it.substringBefore(" ")}
+    val arrayOfNames = array.map{it.substringAfter(" ")}
+    val users = arrayOfLastNames.zip(arrayOfNames)
+    for(user in users) {
+        println("${user.first} ${user.second}")
     }
+
+
+
+
+//    val names = mutableListOf<String>()
+//    val phones = mutableListOf<Long>()
+//    for(i in 0..1000) {
+//        names.add("Имя$i")
+//        phones.add(79_000_000_000 + (Math.random() * 1_000_000_000).toLong())
+//    }
+//    val users = names.zip(phones)
+//    for(user in users) {
+//        println("Имя: ${user.first} Телефон: ${user.second}")
+//    }
 }
 
