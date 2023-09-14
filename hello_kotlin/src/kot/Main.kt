32b2b31fbdddd3cb638a5ee2461185552a0fd0b9
merox-vlados <merox-vlados@mail.ru>
2023-09-14 16:54:21 +0300

    commit 14.09.2023

--- hello_kotlin/src/kot/Main.kt
+++ hello_kotlin/src/kot/Main.kt
@@ -3,24 +3,92 @@ package kot
 
 fun main() {
     val data = mapOf(
-        "file1" to listOf(15,20,45,15,12),
-        "file2" to listOf(35,-20,45,15,12),
-        "file3" to listOf(15,40,45,15,72)
+        "Январь" to listOf(100,100,100,100),
+        "Февраль" to listOf(200,200,-190,200),
+        "Март" to listOf(300,180,300,100),
+        "Апрель" to listOf(250,-250,100,300),
+        "Май" to listOf(200,100,400,300),
+        "Июнь" to listOf(200,100,300,300)
     )
-    val average = data.filter { it.value.all { it >= 0 } }.flatMap { it.value }.average()
-    println(average)
+    printInfo(data)
+}
+
+fun printInfo(data: Map<String, List<Int>>) {
+
+    val validData = data.filterNot { it.value.any{it < 0} }
+    val averageWeek = validData.flatMap { it.value }.average()
+    println("Средняя выручка в неделю: $averageWeek")
+
+    val listOfSum = validData.map{it.value.sum()}
+    val max = listOfSum.max()
+    val min = listOfSum.min()
+    val averageMonth = listOfSum.average()
+
+    val maxMonths = validData.filter { it.value.sum() == max }.keys
+    val minMonths = validData.filter { it.value.sum() == min }.keys
+
+    println("Средняя выручка в месяц: $averageMonth")
+    println("Максимальная выручка в месяц: $max")
+    print("Была в следующих месяцах: ")
+    for (month in maxMonths) {
+        print("$month ")
+    }
+    println("\nМинимальная выручка в месяц: $min")
+    print("Была в следующих месяцах: ")
+    for (month in minMonths) {
+        print("$month ")
+    }
+
+    val invalidData = data.filter { it.value.any{it < 0} }
+    val errorMonths = invalidData.keys
+    print("\nОшибки произошли в следующих месяцах: ")
+    for(month in errorMonths) {
+        print("$month ")
+    }
 
 
-//    val revenueByWeek = listOf(
-//        listOf(8,6,5,1,2),
-//        listOf(4,7,5,8,2),
-//        listOf(8,7,5,1,2),
-//        listOf(8,5,5,1,2)
-//    )
-//    val total = revenueByWeek.flatten()
+//    val averageWeeklyRevenue = data
+//        .filter { it.value.all { it > 0 } }
+//        .flatMap { it.value }.average()
+//    println(averageWeeklyRevenue)
 //
-//    val average = total.average()
-//    println(average)
+//    val averagePerMonthRevenue= data
+//        .filter { it.value.all{it > 0}}
+//        .flatMap { it.value }.sum()
+//    println(averagePerMonthRevenue / 4)
+//
+//    var maxRevenue = 0;
+//    var monthOfMaxRevenue = ""
+//    var minRevenue = 1_000_000_000;
+//    var monthOfMinRevenue = ""
+//    val maxRevenuePerMonth = data
+//        .filter { it.value.all { it > 0 } }
+//    for ((month, revenuePerMonth) in maxRevenuePerMonth) {
+//        val sum = revenuePerMonth.sum()
+//        val month = month
+//        if(maxRevenue < sum) {
+//            maxRevenue = sum
+//            monthOfMaxRevenue = month
+//        }
+//        if(minRevenue > sum) {
+//            minRevenue = sum
+//            monthOfMinRevenue = month
+//        }
+//    }
+//    println(maxRevenue)
+//    println(monthOfMaxRevenue)
+//    println(minRevenue)
+//    println(monthOfMinRevenue)
+//
+//    val errorsInMonths = data
+//        .filter { it.value.any{it < 0} }
+//        .forEach {
+//            println(it.key)
+//        }
+
+
+
+
 
 }
 
