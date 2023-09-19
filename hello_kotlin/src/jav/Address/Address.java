9c4cf665a90823cf819cb9aa6897101e1e9ffa38
merox-vlados <merox-vlados@mail.ru>
2023-09-19 17:29:55 +0300

    commit 19.09.2023

--- hello_kotlin/src/jav/Address/Address.java
+++ hello_kotlin/src/jav/Address/Address.java
@@ -1,5 +1,7 @@
 package jav.Address;
 
+import java.util.Objects;
+
 public class Address {
     private String city;
     private String street;
@@ -34,15 +36,39 @@ public class Address {
     public void setNumberOfHouse(int numberOfHouse) {
         this.numberOfHouse = numberOfHouse;
     }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        if(this == obj) {
+//            return true;
+//        }
+//        if(hashCode() != obj.hashCode()) {
+//            return false;
+//        }
+//        if (obj instanceof Address) {
+//            Address address = (Address) obj;
+//            return this.city.equals(address.city) && this.street.equals(address.street) && this.numberOfHouse == address.numberOfHouse;
+//        } else {
+//            return false;
+//        }
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return city.hashCode() + street.hashCode() + numberOfHouse;
+//    }
+
 
     @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof Address) {
-            Address address = (Address) obj;
-            return this.city.equals(address.city) && this.street.equals(address.street) && this.numberOfHouse == address.numberOfHouse;
-        } else {
-            return false;
-        }
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return numberOfHouse == address.numberOfHouse && Objects.equals(city, address.city) && Objects.equals(street, address.street);
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, numberOfHouse);
+    }
 }
--- hello_kotlin/src/jav/Address/Main.java
+++ hello_kotlin/src/jav/Address/Main.java
@@ -4,11 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Address address1 = new Address("Москва", "Зеленая", 150);
         Address address2 = new Address("Москва", "Зеленая", 150);
-        if(address1.equals(address2)) {
-            System.out.println("Равны");
-        } else {
-            System.out.println("Не равны");
-        }
+
+//        if(address1.equals(address2)) {
+//            System.out.println("Равны");
+//        } else {
+//            System.out.println("Не равны");
+//        }
+
+        System.out.println(address1.hashCode());
+        System.out.println(address2.hashCode());
 
     }
 
