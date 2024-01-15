9a2017b1d08fea437b665ed4a0c60e1e72818669
merox-vlados <merox-vlados@mail.ru>
2024-01-15 22:29:16 +0300

    commit 15.01.2024

--- hello_kotlin_v2/src/main/kotlin/Extension functions.kt
+++ hello_kotlin_v2/src/main/kotlin/Extension functions.kt
@@ -1,30 +1,49 @@
 fun main() {
-    val age = 5
-    if(age.isAgeValid()) {
-        println("Valid")
-    }
-    println(age.isPositive())
 
-    val name = ""
-    if(name.isNotEmpty()) {
-        println("String is not empty")
+    val list = listOf<Int>()
+    myWith(list) {
+        sum()
+        average()
     }
+
+
+
+//    val age = 11
+//    if(age.isAgeValid()) {
+//        println("Valid")
+//    }
+//    println(age.isPositive())
+//
+//    val name = ""
+//    if(name.isNotEmpty()) {
+//        println("String is not empty")
+//    }
+//    if(age.isPrimeNumber()) {
+//        println("number is prime")
+//    } else {
+//        println("number is not prime")
+//    }
+
+}
+
+fun myWith(list: List<Int>, operation: List<Int>.() -> Unit) {
+    list.operation()
 }
 
 //fun isAgeValid(age: Int) = age in 6..100
-
-fun Int.isAgeValid() = this in 6..100
-
-fun Int.isPositive() = this >= 0
-
-fun Int.isPrimeNumber() = run {
-    if(this < 1 ) {
-        false
-    }
-    for(i in 2 until this) {
-        if(this % i == 0) {
-            false
-        }
-    }
-    true
-}
+//
+//fun Int.isAgeValid() = this in 6..100
+//
+//fun Int.isPositive() = this >= 0
+//
+//fun Int.isPrimeNumber() : Boolean {
+//    if(this < 1) {
+//        return false
+//    }
+//    for(i in 2 until this) {
+//        if(this % i == 0) {
+//            return false
+//        }
+//    }
+//    return true
+//}
