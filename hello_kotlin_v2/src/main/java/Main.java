79b72f79d699841b0cc85d975222a35cb6fc0f02
merox-vlados <merox-vlados@mail.ru>
2024-01-18 12:27:59 +0300

    commit 18.01.2024

--- hello_kotlin_v2/src/main/java/Main.java
+++ hello_kotlin_v2/src/main/java/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Address address1 = new Address("Moscow", "Green", 150);
+        Address address2 = new Address("Moscow", "Green", 150);
+
+
+        if(address1.equals(address2)) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not equal");
+        }
+        System.out.println(address1.hashCode());
+        System.out.println(address2.hashCode());
     }
 
 
