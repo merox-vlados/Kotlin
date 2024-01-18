6f819d331e62c4834f19a437726beef5dd4421c7
merox-vlados <merox-vlados@mail.ru>
2024-01-18 22:12:41 +0300

    commit 18.01.2024

--- hello_kotlin_v2/src/main/kotlin/Address/Address.kt
+++ hello_kotlin_v2/src/main/kotlin/Address/Address.kt
@@ -1,5 +1,34 @@
 package Address
 
-data class Address(val city: String, val street: String, val numberOfHouse: Int) {
+class Address(val city: String, val street: String, val numberOfHouse: Int) {
+
+    operator fun component1() = city
+    operator fun component2() = street
+    operator fun component3() = numberOfHouse
+
+    fun copy(city: String = this.city, street: String = this.street, numberOfHouse: Int = this.numberOfHouse): Address {
+        return Address(city, street, numberOfHouse)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Address
+
+        if (city != other.city) return false
+        if (street != other.street) return false
+        if (numberOfHouse != other.numberOfHouse) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = city.hashCode()
+        result = 31 * result + street.hashCode()
+        result = 31 * result + numberOfHouse
+        return result
+    }
+
 
 }
